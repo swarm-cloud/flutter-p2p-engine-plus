ad7beb98dc1f40c52de189fee58aec3e7d893fd5
马嘉伦 <majialun@majialundeMac-Studio.local>
2022-05-24 18:05:04 +0800

    [0.0.1]😘Feat: 添加了安卓的文件

---
+++ swarm_cloud_android/android/src/main/java/com/swarmcloud/sdk/CdnbyeMethodHandler.java
@@ -0,0 +1,267 @@
+package com.cdnbye.cdnbye;
+
+
+import android.app.Activity;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.cdnbye.core.p2p.P2pConfig;
+import com.cdnbye.core.p2p.P2pStatisticsListener;
+import com.cdnbye.core.segment.HlsSegmentIdGenerator;
+import com.cdnbye.core.utils.LogLevel;
+import com.cdnbye.sdk.P2pEngine;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+
+public class CdnbyeMethodHandler implements MethodChannel.MethodCallHandler {
+
+    private Activity activity;
+
+    private volatile String segmentId;
+
+    private static CdnbyeMethodHandler instance;
+
+    private MethodChannel channel;
+
+    public void setActivity(Activity activity) {
+        this.activity = activity;
+    }
+
+    public void setChannel(MethodChannel channel) {
+        this.channel = channel;
+    }
+    private CdnbyeMethodHandler() {
+
+    }
+
+    public void onMethodCall(final MethodCall call, MethodChannel.Result result) {
+        if (call.method.equals("getPlatformVersion")) {
+            // result.success("Android " + android.os.Build.VERSION.RELEASE);
+            result.success(P2pEngine.Version);
+        } else if (call.method.equals("init")) {
+            Map arguments = (Map) call.arguments;
+            String token = (String) arguments.get("token");
+            Map configMap = (Map) arguments.get("config");
+            LogLevel level;
+            switch ((int) configMap.get("logLevel")) {
+                case 2:
+                    level = LogLevel.INFO;
+                    break;
+                case 3:
+                    level = LogLevel.WARN;
+                    break;
+                case 4:
+                    level = LogLevel.ERROR;
+                    break;
+                default:
+                    level = LogLevel.DEBUG;
+            }
+            boolean logEnabled = (int) configMap.get("logLevel") != 0;
+
+            P2pConfig.Builder builder = new P2pConfig.Builder()
+                    .logEnabled(logEnabled).logLevel(level)
+                    .diskCacheLimit((int) configMap.get("diskCacheLimit"))
+                    .memoryCacheCountLimit((int) configMap.get("memoryCacheCountLimit"))
+                    .p2pEnabled((boolean) configMap.get("p2pEnabled"))
+                    .downloadTimeout((int) configMap.get("downloadTimeout"), TimeUnit.SECONDS)
+                    .dcDownloadTimeout((int) configMap.get("dcDownloadTimeout"), TimeUnit.SECONDS)
+                    .localPortHls((int) configMap.get("localPort"))
+                    .maxPeerConnections((int) configMap.get("maxPeerConnections"))
+                    .useHttpRange((boolean) configMap.get("useHttpRange"))
+                    .wifiOnly((boolean) configMap.get("wifiOnly"))
+                    .isSetTopBox((boolean) configMap.get("isSetTopBox"));
+
+            if(configMap.get("wsSignalerAddr") != null)
+                builder= builder.wsSignalerAddr((String) configMap.get("wsSignalerAddr"));
+
+            if(configMap.get("announce") != null)
+                builder = builder.announce((String) configMap.get("announce"));
+
+            if(configMap.get("channelIdPrefix") != null)
+                builder = builder.channelIdPrefix((String) configMap.get("channelIdPrefix"));
+
+            if(configMap.get("tag") != null)
+                builder = builder.withTag((String) configMap.get("tag"));
+
+            if(configMap.get("httpHeaders") != null)
+                builder = builder.httpHeadersForHls((Map<String, String>) configMap.get("httpHeaders"));
+
+            builder = builder.httpLoadTime((long) configMap.get("httpLoadTime"));
+            builder = builder.logPersistent((boolean) configMap.get("logPersistent"));
+            builder = builder.sharePlaylist((boolean) configMap.get("sharePlaylist"));
+            builder = builder.waitForPeer((boolean) configMap.get("waitForPeer"));
+            builder = builder.waitForPeerTimeout((int) configMap.get("waitForPeerTimeout"));
+
+            if(configMap.get("hlsMediaFiles") != null)
+                builder = builder.hlsMediaFiles((ArrayList<String>) configMap.get("hlsMediaFiles"));
+
+            P2pConfig config = builder.build();
+            P2pEngine.init(activity.getApplication().getApplicationContext(), token, config);
+
+            P2pEngine.getInstance().setHlsSegmentIdGenerator(new HlsSegmentIdGenerator() {
+                @Override
+//                public String onSegmentId(int level, long sn, String urlString) {
+                public String onSegmentId(String streamId, long sn, String segmentUrl, String range) {
+                    segmentId = segmentUrl;
+                    Map<String, Object> args = new HashMap<>();
+                    args.put("sn", sn);
+                    args.put("segmentUrl", segmentUrl);
+                    args.put("streamId", streamId);
+                    args.put("range", range);
+
+                    CountDownLatch latch = new CountDownLatch(1);
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            channel.invokeMethod("segmentId", args, new MethodChannel.Result() {
+                                @Override
+                                public void success(@Nullable Object result) {
+//                                    System.out.println("native result: " + result);
+                                    if (result != null) {
+                                        Map map = (Map<String, String>) result;
+                                        segmentId = (String) map.get("result");
+                                       System.out.println(segmentId);
+                                    }
+                                    latch.countDown();
+                                }
+
+                                @Override
+                                public void error(String errorCode, @Nullable String errorMessage, @Nullable Object errorDetails) {
+                                    latch.countDown();
+                                }
+
+                                @Override
+                                public void notImplemented() {
+                                    latch.countDown();
+                                }
+                            });
+                        }
+                    });
+                    try {
+                        latch.await(100, TimeUnit.MILLISECONDS);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+//                    System.out.println("native segmentId: " + segmentId);
+                    return segmentId;
+                }
+            });
+
+            result.success(1);
+        } else if (call.method.equals("parseStreamURL")) {
+            Map configMap = (Map) call.arguments;
+            String url = (String) configMap.get("url");
+            String videoId = (String) configMap.get("videoId");
+            String parsedUrl = "";
+            if(videoId== null){
+                parsedUrl = P2pEngine.getInstance().parseStreamUrl(url);
+            }else{
+                parsedUrl = P2pEngine.getInstance().parseStreamUrl(url, videoId);
+            }
+
+            result.success(parsedUrl);
+        } else if (call.method.equals("startListen")) {
+            P2pEngine.getInstance().addP2pStatisticsListener(new P2pStatisticsListener() {
+                @Override
+                public void onHttpDownloaded(final long value) {
+                    final Map info = new HashMap();
+                    info.put("httpDownloaded", value);
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            channel.invokeMethod("info", info);
+                        }
+                    });
+                }
+
+                @Override
+                public void onP2pDownloaded(final long value, final int value2) {
+                    final Map info = new HashMap();
+                    info.put("p2pDownloaded", value);
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            channel.invokeMethod("info", info);
+                        }
+                    });
+                }
+
+                @Override
+                public void onP2pUploaded(final long value) {
+                    final Map info = new HashMap();
+                    info.put("p2pUploaded", value);
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            channel.invokeMethod("info", info);
+                        }
+                    });
+                }
+
+                @Override
+                public void onPeers(List<String> peers) {
+                    final Map info = new HashMap();
+                    info.put("peers", peers);
+                    activity.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            channel.invokeMethod("info", info);
+                        }
+                    });
+
+                }
+
+                @Override
+                public void onServerConnected(boolean connected) {
+                   
+                }
+            });
+            result.success(1);
+        } else if (call.method.equals("isConnected")) {
+            result.success(P2pEngine.getInstance().isConnected());
+        } else if (call.method.equals("restartP2p")) {
+            try {
+                Map arguments = (Map) call.arguments;
+                String url = (String) arguments.get("url");
+                if (url == null) {
+                    result.error("No Url","Fail to restart p2p without url.",null);
+                    return;
+                }
+                P2pEngine.getInstance().restartP2p(this.activity.getApplicationContext(),new URL(url));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            result.success(1);
+        } else if (call.method.equals("stopP2p")) {
+            P2pEngine.getInstance().stopP2p();
+            result.success(1);
+        } else if (call.method.equals("getPeerId")) {
+            result.success(P2pEngine.getInstance().getPeerId());
+        }
+
+        else {
+            System.out.println("notImplemented");
+            result.notImplemented();
+        }
+    }
+
+    public static CdnbyeMethodHandler getInstance() {
+
+        if (instance == null) {
+            instance = new CdnbyeMethodHandler();
+        }
+
+        return instance;
+    }
+}
---
+++ swarm_cloud_android/android/src/main/java/com/swarmcloud/sdk/CdnbyePlugin.java
@@ -0,0 +1,63 @@
+package com.cdnbye.cdnbye;
+
+import android.app.Activity;
+import android.app.Application;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
+import io.flutter.plugin.common.EventChannel;
+import io.flutter.plugin.common.EventChannel.EventSink;
+import io.flutter.plugin.common.EventChannel.StreamHandler;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.embedding.engine.plugins.activity.ActivityAware;
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
+
+// import com.cdnbye.sdk.*;
+import com.cdnbye.cdnbye.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+
+/** CdnbyePlugin */
+public class CdnbyePlugin implements ActivityAware, FlutterPlugin {
+
+  CdnbyeMethodHandler handler = CdnbyeMethodHandler.getInstance();
+  @Override
+  public void onDetachedFromEngine(FlutterPluginBinding binding) {
+  }
+
+  @Override
+  public void onAttachedToEngine(FlutterPluginBinding binding) {
+    final MethodChannel channel = new MethodChannel(binding.getFlutterEngine().getDartExecutor(), "cdnbye");
+    handler.setChannel(channel);
+    channel.setMethodCallHandler(handler);
+  }
+
+  @Override
+  public void onAttachedToActivity(ActivityPluginBinding binding) {
+    handler.setActivity(binding.getActivity());
+  }
+
+  @Override
+  public void onReattachedToActivityForConfigChanges(ActivityPluginBinding binding) {
+    handler.setActivity(binding.getActivity());
+  }
+
+  @Override
+  public void onDetachedFromActivityForConfigChanges() {
+
+  }
+
+  @Override
+  public void onDetachedFromActivity() {
+
+  }
+
+
+}
